bce4410db76435055b4079bdd4b57c813412eb81
Ben Collins <aggieben@gmail.com>
2022-06-23 01:14:30 -0500

    first stab.

--- src/FsMatrix/Library.fs
+++ src/FsMatrix/Library.fs
@@ -1,5 +1,25 @@
 ﻿namespace FsMatrix
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type MatrixError = {
+    errcode: string
+    error: string
+}
+
+module Matrix =
+
+    open System.Text.Json
+    open System.Text.Json.Serialization
+    open FsHttp
+    
+    GlobalConfig.Json.defaultJsonSerializerOptions <-
+        let options = JsonSerializerOptions()
+        options.Converters.Add(JsonFSharpConverter())
+        options
+
+    let getBaseUrlFromHomeserverName name =
+        http {
+            GET (sprintf "https://%s/.well-known/matrix/client" name) 
+        }
+        |> Request.sendAsync
+        |> Async.map Response.toJson
+        |> Async.map (fun json -> (json?``m.homeserver``).GetString())
